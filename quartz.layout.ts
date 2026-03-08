6b0edf0781aba8e1cf906db72a4c0b59824e4432
ShikariDada <87100171+ShikariDada@users.noreply.github.com>
2026-03-08 12:44:16 +0530

    Update footer links in quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Instagram: "https://www.instagram.com/zerothlayer?igsh=OGF2ZnowdG1hbWhq",
+      "Discord Community": "https://discord.gg/Uzs63XGuS",
     },
   }),
 }
