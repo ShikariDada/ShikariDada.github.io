2d112543f6177d364e626626351c608fe0d16ba3
ShikariDada <87100171+ShikariDada@users.noreply.github.com>
2026-03-09 16:53:33 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,25 +26,33 @@ export const sharedPageComponents: SharedLayout = {
 // ==========================================
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    // Component.ConditionalRender({
-    //   component: Component.Breadcrumbs(),
-    //   condition: (page) => page.fileData.slug !== "index",
-    // }),
     Component.ConditionalRender({
       component: Component.ArticleTitle(),
       condition: (page) => page.fileData.slug !== "index",
     }),
-    Component.ContentMeta(),
-    Component.TagList(),
+    Component.ConditionalRender({
+      component: Component.ContentMeta(),
+      condition: (page) => page.fileData.slug !== "index", /* Hides date/read time on homepage */
+    }),
+    Component.ConditionalRender({
+      component: Component.TagList(),
+      condition: (page) => page.fileData.slug !== "index", /* Hides default tags on homepage */
+    }),
   ],
   left: [],
   right: [],
   afterBody: [
-    Component.Graph({
-      localGraph: { depth: 1, linkDistance: 30 },
-      globalGraph: { depth: 2 },
+    Component.ConditionalRender({
+      component: Component.Graph({
+        localGraph: { depth: 1, linkDistance: 30 },
+        globalGraph: { depth: 2 },
+      }),
+      condition: (page) => page.fileData.slug !== "index", /* Hides the graph on homepage */
     }),
-    Component.Backlinks(),
+    Component.ConditionalRender({
+      component: Component.Backlinks(),
+      condition: (page) => page.fileData.slug !== "index", /* Hides backlinks on homepage */
+    }),
   ],
 }
 
